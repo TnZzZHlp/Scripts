77ef6f0347d1354c51df90f561814162de3666b1
TnZzZHlp <tnzzzhlp@outlook.com>
2025-06-10 15:26:35 +0800

    fix(downloadKemono): 修正代理协议为 socks4，确保代理连接正常

--- src/Video/downloadKemono.py
+++ src/Video/downloadKemono.py
@@ -6,7 +6,7 @@ from aiohttp_socks import ProxyConnector
 
 DOMAIN = None
 SEM = asyncio.Semaphore(2)  # 限制并发下载数量
-PROXY = "socks4a://192.168.2.1:7890"
+PROXY = "socks4://192.168.2.1:7890"
 
 
 def parse_artist_url(url: str) -> list:
