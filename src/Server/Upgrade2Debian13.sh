6b58beb325d997c1fe48a159626a450f207f98bf
TnZzZHlp <tnzzzhlp@outlook.com>
2025-08-06 12:06:23 +0800

    feat: 更新 Debian 13 的软件源配置，优化升级脚本

--- src/Server/Upgrade2Debian13.sh
+++ src/Server/Upgrade2Debian13.sh
@@ -1,54 +1,62 @@
 #!/bin/bash
 
-apt update
-apt upgrade -y
-apt full-upgrade -y
-apt autoclean
-apt autoremove -y
+# 更新 Debian 13 的软件源
+update_sources() {
+    echo "正在更新 Debian 13 的软件源..."
+    echo 'Types: deb
+    URIs: https://deb.debian.org/debian
+    Suites: bookworm bookworm-updates bookworm-backports
+    Components: main contrib non-free non-free-firmware
+    Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
-echo 'Types: deb
-URIs: https://deb.debian.org/debian
-Suites: bookworm bookworm-updates bookworm-backports
-Components: main contrib non-free non-free-firmware
-Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+    # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+    # Types: deb-src
+    # URIs: https://deb.debian.org/debian
+    # Suites: bookworm bookworm-updates bookworm-backports
+    # Components: main contrib non-free non-free-firmware
+    # Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
-# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
-# Types: deb-src
-# URIs: https://deb.debian.org/debian
-# Suites: bookworm bookworm-updates bookworm-backports
-# Components: main contrib non-free non-free-firmware
-# Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+    # 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
+    # Types: deb
+    # URIs: https://deb.debian.org/debian-security
+    # Suites: bookworm-security
+    # Components: main contrib non-free non-free-firmware
+    # Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
-# 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
-# Types: deb
-# URIs: https://deb.debian.org/debian-security
-# Suites: bookworm-security
-# Components: main contrib non-free non-free-firmware
-# Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+    # # Types: deb-src
+    # # URIs: https://deb.debian.org/debian-security
+    # # Suites: bookworm-security
+    # # Components: main contrib non-free non-free-firmware
+    # # Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
-# # Types: deb-src
-# # URIs: https://deb.debian.org/debian-security
-# # Suites: bookworm-security
-# # Components: main contrib non-free non-free-firmware
-# # Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+    Types: deb
+    URIs: http://security.debian.org/debian-security
+    Suites: bookworm-security
+    Components: main contrib non-free non-free-firmware
+    Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
-Types: deb
-URIs: http://security.debian.org/debian-security
-Suites: bookworm-security
-Components: main contrib non-free non-free-firmware
-Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+    # Types: deb-src
+    # URIs: http://security.debian.org/debian-security
+    # Suites: bookworm-security
+    # Components: main contrib non-free non-free-firmware
+    # Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+    ' | sudo tee /etc/apt/sources.list.d/debian.sources
 
-# Types: deb-src
-# URIs: http://security.debian.org/debian-security
-# Suites: bookworm-security
-# Components: main contrib non-free non-free-firmware
-# Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
-' | sudo tee /etc/apt/sources.list.d/debian.sources
+    mv /etc/apt/sources.list /etc/apt/sources.list.bak
 
-mv /etc/apt/sources.list /etc/apt/sources.list.bak
+    echo "Debian 13 的软件源更新完成！"
+}
 
-apt update
-apt upgrade -y
-apt full-upgrade -y
-apt autoclean
-apt autoremove -y
+upgrade_system() {
+    echo "正在升级系统..."
+    apt update
+    apt upgrade -y
+    apt full-upgrade -y
+    apt autoclean
+    apt autoremove -y
+    echo "系统升级完成！"
+}
+
+upgrade_system
+update_sources
+upgrade_system
