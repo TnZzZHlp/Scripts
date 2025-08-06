cd8a65f2b6c7bd977ecf8cdcf9de1028166fd881
TnZzZHlp <tnzzzhlp@outlook.com>
2025-08-06 12:02:18 +0800

    feat: 添加升级 Debian 13 的脚本

---
+++ src/Server/Upgrade2Debian13.sh
@@ -0,0 +1,54 @@
+!#/bin/bash
+
+apt update
+apt upgrade -y
+apt full-upgrade -y
+apt autoclean
+apt autoremove -y
+
+echo 'Types: deb
+URIs: https://deb.debian.org/debian
+Suites: bookworm bookworm-updates bookworm-backports
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+# Types: deb-src
+# URIs: https://deb.debian.org/debian
+# Suites: bookworm bookworm-updates bookworm-backports
+# Components: main contrib non-free non-free-firmware
+# Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+# 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
+# Types: deb
+# URIs: https://deb.debian.org/debian-security
+# Suites: bookworm-security
+# Components: main contrib non-free non-free-firmware
+# Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+# # Types: deb-src
+# # URIs: https://deb.debian.org/debian-security
+# # Suites: bookworm-security
+# # Components: main contrib non-free non-free-firmware
+# # Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+Types: deb
+URIs: http://security.debian.org/debian-security
+Suites: bookworm-security
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+# Types: deb-src
+# URIs: http://security.debian.org/debian-security
+# Suites: bookworm-security
+# Components: main contrib non-free non-free-firmware
+# Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+' | sudo tee /etc/apt/sources.list.d/debian.sources
+
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+
+apt update
+apt upgrade -y
+apt full-upgrade -y
+apt autoclean
+apt autoremove -y
