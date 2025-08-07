f101803c2e415b871377702d5b709508dbd33def
TnZzZHlp <tnzzzhlp@outlook.com>
2025-08-07 10:13:04 +0800

    Update Upgrade2Debian13.sh

--- src/Server/Upgrade2Debian13.sh
+++ src/Server/Upgrade2Debian13.sh
@@ -5,7 +5,7 @@ update_sources() {
     echo "正在更新 Debian 13 的软件源..."
     echo 'Types: deb
 URIs: https://deb.debian.org/debian
-Suites: bookworm bookworm-updates bookworm-backports
+Suites: trixie trixie-updates trixie-backports
 Components: main contrib non-free non-free-firmware
 Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
@@ -17,29 +17,42 @@ Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 # Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
 # 以下安全更新软件源包含了官方源与镜像站配置，如有需要可自行修改注释切换
-# Types: deb
-# URIs: https://deb.debian.org/debian-security
-# Suites: bookworm-security
-# Components: main contrib non-free non-free-firmware
-# Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+Types: deb
+URIs: https://deb.debian.org/debian-security
+Suites: trixie-security
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
 # # Types: deb-src
 # # URIs: https://deb.debian.org/debian-security
-# # Suites: bookworm-security
+# # Suites: trixie-security
 # # Components: main contrib non-free non-free-firmware
 # # Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
 Types: deb
-URIs: http://security.debian.org/debian-security
-Suites: bookworm-security
+URIs: https://deb.debian.org/debian
+Suites: trixie-backports
 Components: main contrib non-free non-free-firmware
 Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 
 # Types: deb-src
-# URIs: http://security.debian.org/debian-security
-# Suites: bookworm-security
+# URIs: https://deb.debian.org/debian
+# Suites: trixie-backports
 # Components: main contrib non-free non-free-firmware
 # Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+Types: deb
+URIs: https://deb.debian.org/debian
+Suites: trixie-updates
+Components: main contrib non-free non-free-firmware
+Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
+# Types: deb-src
+# URIs: https://deb.debian.org/debian
+# Suites: trixie-updates
+# Components: main contrib non-free non-free-firmware
+# Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
+
 ' | sudo tee /etc/apt/sources.list.d/debian.sources
 
     if [ $? -ne 0 ]; then
