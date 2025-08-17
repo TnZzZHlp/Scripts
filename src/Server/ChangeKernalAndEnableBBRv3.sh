1255950fb63a4e6906c5ba27703a962fc5bf3277
TnZzZHlp <tnzzzhlp@outlook.com>
2025-08-17 14:16:49 +0800

    feat: 添加脚本以更改内核并启用 BBR，配置网络参数

---
+++ src/Server/ChangeKernalAndEnableBBRv3.sh
@@ -0,0 +1,21 @@
+# 创建并写入 DEB822 格式的源文件
+echo "Types: deb
+URIs: https://deb.xanmod.org
+Suites: releases
+Components: main
+Signed-By: /etc/apt/keyrings/xanmod-archive-keyring.gpg" | sudo tee /etc/apt/sources.list.d/xanmod.sources > /dev/null
+
+sudo apt update -y
+sudo apt install linux-xanmod-x64v3 -y
+
+echo "
+net.ipv4.ip_forward = 1
+net.ipv6.conf.all.forwarding = 1
+net.core.default_qdisc = fq_pie
+net.ipv4.tcp_congestion_control = bbr
+net.ipv4.tcp_rmem = 4096 87380 33554432
+net.ipv4.tcp_wmem = 4096 87380 33554432
+" | tee /etc/sysctl.d/99-sysctl.conf > /dev/null
+
+# 应用配置
+sysctl --system
